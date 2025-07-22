4e3879802e3fd221f3dec1f947c79cb4695e994a
Morty <martin@familie-bosche.de>
2025-07-22 14:16:21 +0200

    Release v1

---
+++ DownloadHistoryWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using MortysDLP.Models;
+using MortysDLP.Services;
+using System.Windows;
+
+namespace MortysDLP
+{
+    /// <summary>
+    /// Interaktionslogik für DownloadHistoryWindow.xaml
+    /// </summary>
+    public partial class DownloadHistoryWindow : Window
+    {
+        public DownloadHistoryWindow()
+        {
+            InitializeComponent();
+            LoadHistory();
+        }
+
+        private void LoadHistory()
+        {
+            HistoryList.ItemsSource = DownloadHistoryService.Load();
+            UpdateButtonStates();
+        }
+
+        private void Clear_Click(object sender, RoutedEventArgs e)
+        {
+            DownloadHistoryService.Clear();
+            LoadHistory();
+        }
+
+        private void Reuse_Click(object sender, RoutedEventArgs e)
+        {
+            if (HistoryList.SelectedItem is DownloadHistoryEntry entry)
+            {
+                // Hauptfenster suchen und URL setzen
+                foreach (Window window in Application.Current.Windows)
+                {
+                    if (window is Hauptfenster main)
+                    {
+                        main.tb_URL.Text = entry.Url;
+                        main.Activate();
+                        break;
+                    }
+                }
+                this.Close();
+            }
+        }
+
+        private void UpdateButtonStates()
+        {
+            bool hasItems = HistoryList.Items.Count > 0;
+            ReuseButton.IsEnabled = hasItems;
+            ClearButton.IsEnabled = hasItems;
+            EmptyText.Visibility = hasItems ? Visibility.Collapsed : Visibility.Visible;
+        }
+    }
+}
---
+++ DownloadProgressDialog.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MortysDLP
+{
+    /// <summary>
+    /// Interaktionslogik für DownloadProgressDialog.xaml
+    /// </summary>
+    public partial class DownloadProgressDialog : Window
+    {
+        public DownloadProgressDialog(string info)
+        {
+            InitializeComponent();
+            InfoText.Text = info;
+            ProgressBar.Value = 0;
+        }
+
+        public void SetProgress(double value)
+        {
+            ProgressBar.Value = value * 100;
+        }
+    }
+}
