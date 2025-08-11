99a91ff2897eecdbcbef5f7bba010d754155e8c6
Martin Bosche <mbo@gneuss.de>
2025-08-11 10:12:40 +0200

    Settings Fix

--- Properties/Settings.Designer.cs
+++ Properties/Settings.Designer.cs
@@ -289,7 +289,7 @@ namespace MortysDLP.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        [global::System.Configuration.DefaultSettingValueAttribute("True")]
         public bool ForceEnglishLanguage {
             get {
                 return ((bool)(this["ForceEnglishLanguage"]));
